4580f3a81cc1de1d04a57c22f45852d3d712b6b4
HerrSVz20 <alvaro.arn2015@gmail.com>
2023-05-03 20:49:49 -0400

    migrationlogin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,7 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->get('/calendario', 'Home::calendario');
+service('auth')->routes($routes);
 
 /*
  * --------------------------------------------------------------------
