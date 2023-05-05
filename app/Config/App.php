c921722f15249acff85b631a3f1772d70b629492
HerrSVz20 <alvaro.arn2015@gmail.com>
2023-05-04 20:47:06 -0400

    corregidoCalendario

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://ci10.stacknews.tech/public/';
+    public string $baseURL = 'http://localhost/desarrolocode/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
